12e17f0f3954def3e539f270a58f5dbd4608057c
Jin Igarashi <igarashi.jin@water-gis.com>
2024-01-09 20:13:24 +0000

    lab1 part 2 my initials

--- algorighms_and_data_structures/Java/Week1 Labs/lab1/Hello/Hello.java
+++ algorighms_and_data_structures/Java/Week1 Labs/lab1/Hello/Hello.java
@@ -7,5 +7,16 @@ class Hello {
         public static void main(String[] args) {
                 System.out.println("Hello, welcome to the University of York");
                 //put new code here
+                System.out.println("My initials");
+                System.out.println("---------------------");
+                System.out.println("*********************");
+                System.out.println("*****        ****  **");
+                System.out.println("********  *******  **");
+                System.out.println("********  *******  **");
+                System.out.println("********  *******  **");
+                System.out.println("********  *******  **");
+                System.out.println("*  *****  *******  **");
+                System.out.println("**       ********  **");
+                System.out.println("*********************");
         }
 }
