cd3f9ec9c2793644b2285bff5d4074ba5234ed01
Jin Igarashi <igarashi.jin@water-gis.com>
2024-01-10 08:47:29 +0000

    1.2.9

--- algorighms_and_data_structures/Java/Week1 Labs/lab2/Starfleet2/TestSpaceship2.java
+++ algorighms_and_data_structures/Java/Week1 Labs/lab2/Starfleet2/TestSpaceship2.java
@@ -4,7 +4,8 @@ public class TestSpaceship2 {
 	public static void main(String[] args) {
 		//write your code here, 
 		//create a Spaceship2 object, call the displaySettings method
-
+		SpaceShip2 s = new SpaceShip2();
+		s.displaySettings();
 	}
 
 }
